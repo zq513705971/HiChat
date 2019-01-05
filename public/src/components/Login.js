7d69adfcff7f30cd53b48704bf912cd28fec8459
zhanqun <513705971@qq.com>
2019-01-05 16:01:14 +0800

    增加用户注册和忘记密码页面

--- public/src/components/Login.js
+++ public/src/components/Login.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { observer, inject } from "mobx-react";
+import Register from './Register';
+import ForgetPassword from './ForgetPassword';
 
 @observer // 监听当前组件
 export default class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            store: this.props.store
+            store: this.props.store,
+            register: false,
+            forget: false
         };
     }
 
@@ -25,33 +29,62 @@ export default class Login extends React.Component {
         document.removeEventListener("keydown", this._handleEnterKey);
     }
 
+    _register = () => {
+        this.setState({
+            register: true
+        });
+    }
+
+    _forget = () => {
+        this.setState({
+            forget: true
+        });
+    }
+
     render() {
-        var { store } = this.state;
+        var { store, register, forget } = this.state;
         var { userId, password } = store;
         return (
             <div className="login-window">
-                <div className="login-form">
-                    <div className="header">HiChat! :)</div>
-                    <div className="input">
-                        <input type="text" value={userId} placeholder="请输入用户名"
-                            onChange={(e) => {
-                                store.userId = e.target.value;
-                            }}
-                        />
-                    </div>
-                    <div className="input">
-                        <input type="password" value={password} placeholder="请输入密码"
-                            onChange={(e) => {
-                                store.password = e.target.value;
-                            }}
-                        />
-                    </div>
-                    <div className="btn">
-                        <input type="button" value="登录" onClick={() => {
-                            store.login();
-                        }} />
-                    </div>
-                </div>
+                {
+                    !register && !forget ?
+                        <div className="login-form">
+                            <div className="header">HiChat! :)</div>
+                            <div className="input">
+                                <input type="text" value={userId} placeholder="请输入用户名"
+                                    onChange={(e) => {
+                                        store.userId = e.target.value;
+                                    }}
+                                />
+                            </div>
+                            <div className="input">
+                                <input type="password" value={password} placeholder="请输入密码"
+                                    onChange={(e) => {
+                                        store.password = e.target.value;
+                                    }}
+                                />
+                            </div>
+                            <div className="btn">
+                                <input type="button" value="登录" onClick={() => {
+                                    store.login();
+                                }} />
+                            </div>
+                            <div className="other">
+                                <div className="register" onClick={this._register}>注册</div>
+                                <div className="forget" onClick={this._forget}>忘记密码</div>
+                            </div>
+                        </div> :
+                        register ? <Register store={store} onClose={() => {
+                            this.setState({
+                                register: false
+                            });
+                        }} /> :
+                            <ForgetPassword store={store} onClose={() => {
+                                this.setState({
+                                    forget: false
+                                });
+                            }} />
+                }
             </div>
         );
     }
---
+++ public/src/components/Register.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { toJS } from 'mobx';
+import { observer, inject } from "mobx-react";
+
+@observer // 监听当前组件
+export default class Register extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        var { store } = this.props;
+        var { image, name } = store;
+        return (
+            <div className="register-form">
+                <div className="header">
+                    <div className="title">注册</div>
+                    <div className="close" onClick={() => {
+                        this.props.onClose && this.props.onClose();
+                    }}>×</div>
+                </div>
+                <div className="block">
+                    <div className="input">
+                        <input type="text" placeholder="请输入用户名." value="" />
+                    </div>
+                    <div className="input">
+                        <input type="password" placeholder="请输入密码." value="" />
+                    </div>
+                    <div className="input">
+                        <input type="password" placeholder="请输入确认密码." value="" />
+                    </div>
+                    <div className="btn">
+                        <input type="button" value="注册" onClick={() => {
+
+                        }} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
